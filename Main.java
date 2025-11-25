1ba4c240da0d62f59b9199fb3b0892019a9c037c
RAM <ganeshram2921@gmail.com>
2025-11-25 13:12:51 +0530

    Print array elements individually

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ package myPackage;
 public class Main{
     public static void main(String args[]){
         int arr[] = {12,3,56,26,4,45,23,74,34,6};
-        System.out.println(arr);
+        for(int i : arr){
+            System.out.println(i + " ");
+        }
     }
 }
