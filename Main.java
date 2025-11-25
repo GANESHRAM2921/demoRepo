855a8d68659f613123af4a452836a941e55ac8db
GANESHRAM2921 <ganeshram2921@gmail.com>
2025-11-25 17:34:16 +0530

    Docs: added a comment

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-package myPackage;
 public class Main{
     public static void main(String args[]){
         int arr[] = {12,3,56,26,4,45,23,74,34,6};
         for(int i : arr){
             System.out.println(i + " " + 2 * i);
             // printing elements with it's product with 2
+            // conflicting
         }
     }
 }
