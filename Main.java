f6d56421aeecd513d386973e5a5acdceaf092fe9
GANESHRAM2921 <ganeshram2921@gmail.com>
2025-11-25 17:22:16 +0530

    Fix: changed some code

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
-package myPackage;
+
 public class Main{
     public static void main(String args[]){
         int arr[] = {12,3,56,26,4,45,23,74,34,6};
         for(int i : arr){
-            System.out.println(i + " " + 2 * i);
+            System.out.println(i + " ");
             // printing elements with it's product with 2
             // bye brownikha
         }
